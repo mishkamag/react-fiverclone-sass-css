5156ec34d10c507f16ee250edcdd4aac3e3829c2
mishkamag <m93.maglaperidze@gmail.com>
2023-04-22 21:26:59 +0400

    working with navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,26 @@ import React from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <span className="text">fiver</span>
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <ul>
+            <li>Fiverr Business</li>
+            <li>Explore</li>
+            <li>English</li>
+            <li>Sign In</li>
+            <li>Become a Seller</li>
+            <button>Join</button>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
