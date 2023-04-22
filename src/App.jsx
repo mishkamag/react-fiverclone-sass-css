676be9a9ec08bb34d9cae8071c0ea6295ce5a160
mishkamag <m93.maglaperidze@gmail.com>
2023-04-22 23:40:15 +0400

    finished navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,204 @@ const App = () => {
   return (
     <div>
       <Navbar />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        repellat aspernatur saepe facere, libero ut, nam quaerat eum distinctio,
+        aliquam officia ducimus deserunt fugit suscipit mollitia sit corporis
+        odio? Magnam?
+      </p>
     </div>
   );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
+// import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [options, setOptions] = useState(false);
+
+  const handleScroll = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "mishka",
+    isSeller: true,
+  };
+
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
+          {/* <Link to="/"> */}
           <span className="text">fiver</span>
           <span className="dot">.</span>
+          {/* </Link> */}
         </div>
         <div className="links">
           <ul>
@@ -15,11 +36,47 @@ const Navbar = () => {
             <li>Explore</li>
             <li>English</li>
             <li>Sign In</li>
-            <li>Become a Seller</li>
-            <button>Join</button>
+            {!currentUser?.isSeller && <li>Become a Seller</li>}
+            {!currentUser && <button>Join</button>}
+            {currentUser && (
+              <div
+                className="user"
+                onClick={() => {
+                  setOptions(!options);
+                }}
+              >
+                <img
+                  src="https://scontent.ftbs6-2.fna.fbcdn.net/v/t1.6435-9/71698868_1100255983510799_7974481038502002688_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=3aTnWV0TqXsAX8i2TAh&_nc_ht=scontent.ftbs6-2.fna&oh=00_AfClRm2YQkJoiyv7kpSDD86NIjT1KyTuBhR18b6bDFLxQQ&oe=646BADAC"
+                  alt=""
+                />
+                <span>{currentUser?.username}</span>
+                {options && (
+                  <div className="options">
+                    {currentUser?.isSeller && (
+                      <>
+                        <span>Gigs</span>
+                        <span>Add New Gig</span>
+                      </>
+                    )}
+                    <span>Orders</span>
+                    <span>Messages</span>
+                    <span>Logout</span>
+                  </div>
+                )}
+              </div>
+            )}
           </ul>
         </div>
       </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test1</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
