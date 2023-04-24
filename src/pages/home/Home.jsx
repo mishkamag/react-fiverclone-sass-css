629ae9fff3519d226fe6b68d9bcba83e0dfb9ec4
mishkamag <m93.maglaperidze@gmail.com>
2023-04-25 00:51:55 +0400

    finished infinity carusel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import "./Home.scss";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slider/Slide";
+import { cards } from "../../data";
+import CatCard from "../../components/catCard/CatCard";
 
 const Home = () => {
   return (
     <div>
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {cards.map((card) => (
+          <CatCard item={card} key={card.id} />
+        ))}{" "}
+      </Slide>
     </div>
   );
 };
