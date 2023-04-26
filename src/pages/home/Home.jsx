1847398f5ecd1782d7f5257889bba3902628d57e
mishkamag <m93.maglaperidze@gmail.com>
2023-04-27 00:38:05 +0400

    impliment other feature

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,10 +5,11 @@ import TrustedBy from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/slider/Slide";
 import { cards } from "../../data";
 import CatCard from "../../components/catCard/CatCard";
+import { AiOutlineCheckCircle } from "react-icons/ai";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <Featured />
       <TrustedBy />
       <Slide slidesToShow={4} arrowsScroll={4}>
@@ -16,6 +17,46 @@ const Home = () => {
           <CatCard item={card} key={card.id} />
         ))}{" "}
       </Slide>
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1>The best part? Everything.</h1>
+            <AiOutlineCheckCircle />
+            Stick to your budget
+            <p>
+              Find the right service for every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+          </div>
+          <div className="item">
+            <h1>The best part? Everything.</h1>
+            <AiOutlineCheckCircle />
+            Stick to your budget
+            <p>
+              Find the right service for every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+          </div>
+          <div className="item">
+            <h1>The best part? Everything.</h1>
+            <AiOutlineCheckCircle />
+            Stick to your budget
+            <p>
+              Find the right service for every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+          </div>
+          <div className="item">
+            <h1>The best part? Everything.</h1>
+            <AiOutlineCheckCircle />
+            Stick to your budget
+            <p>
+              Find the right service for every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
