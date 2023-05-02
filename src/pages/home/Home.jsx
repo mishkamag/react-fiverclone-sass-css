0d5f2a9a54434faaf9b6dcd7de769c3e535e00f6
mishkamag <m93.maglaperidze@gmail.com>
2023-05-02 15:27:13 +0400

    finished home's feature component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,45 +15,90 @@ const Home = () => {
       <Slide slidesToShow={4} arrowsScroll={4}>
         {cards.map((card) => (
           <CatCard item={card} key={card.id} />
-        ))}{" "}
+        ))}
       </Slide>
       <div className="features">
         <div className="container">
           <div className="item">
             <h1>The best part? Everything.</h1>
+            <div className="title">
             <AiOutlineCheckCircle />
             Stick to your budget
+            </div>
             <p>
               Find the right service for every price point. No hourly rates,
               just project-based pricing.
             </p>
+          
+          <div className="title">
+           <AiOutlineCheckCircle />
+           Get quality work done quickly
+           </div>
+            <p>
+              Find the right service for every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+         
+          <div className="title">
+              <AiOutlineCheckCircle />
+              Pay when you're happy
+              </div>
+            <p>
+              Find the right service for every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+          
+          <div className="title">
+            <AiOutlineCheckCircle />
+            Count on 24/7 support
+            </div>
+            <p>
+              Find the right service for every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+       
+       
           </div>
           <div className="item">
-            <h1>The best part? Everything.</h1>
-            <AiOutlineCheckCircle />
-            Stick to your budget
-            <p>
-              Find the right service for every price point. No hourly rates,
-              just project-based pricing.
-            </p>
+            <video src="../../../img/video.mp4"  controls/>
           </div>
+        </div>
+      </div>
+
+      <div className="features dark">
+        <div className="container">
           <div className="item">
-            <h1>The best part? Everything.</h1>
+            <h1>fiverr business</h1>
+            <h1>A solution built for <i>business</i></h1>
+            <p>Upgrade to a curated experience to access vetted talent and exclusive tools</p>
+        
+
+          <div className="title">
             <AiOutlineCheckCircle />
-            Stick to your budget
-            <p>
-              Find the right service for every price point. No hourly rates,
-              just project-based pricing.
-            </p>
+            Talent matching
           </div>
-          <div className="item">
-            <h1>The best part? Everything.</h1>
+          
+          <div className="title">
             <AiOutlineCheckCircle />
-            Stick to your budget
-            <p>
-              Find the right service for every price point. No hourly rates,
-              just project-based pricing.
-            </p>
+            Dedicated account management
+          </div>
+
+          
+          <div className="title">
+            <AiOutlineCheckCircle />
+            Team collaboration tools
+          </div>
+
+          
+          <div className="title">
+            <AiOutlineCheckCircle />
+            Business payment solutions
+          </div>
+          <button>Explore Fiverr Business</button>
+          </div>
+          
+          <div className="item">
+            <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624757/business-desktop-870-x1.png" alt="" />
           </div>
         </div>
       </div>
