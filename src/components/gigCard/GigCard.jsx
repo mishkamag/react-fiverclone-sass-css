300df4e9773a90ac25ba85fb20f12d4fc6ad106b
mishkamag <m93.maglaperidze@gmail.com>
2023-05-03 23:59:17 +0400

    finished category page

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -1,36 +1,33 @@
-import React from 'react'
-import "./GigCard.scss"
-import { Link } from "react-router-dom"
-import { AiOutlineHeart } from 'react-icons/ai';
+import React from "react";
+import "./GigCard.scss";
+import { Link } from "react-router-dom";
+import { AiOutlineHeart, AiOutlineStar } from "react-icons/ai";
 
-const GigCard = ({item}) => {
+const GigCard = ({ item }) => {
   return (
-    <Link to="/gig/123">
-    <div className='gigCard'>
+    <Link to="/gig/123" className="link">
+      <div className="gigCard">
         <img src={item.img} alt="" />
         <div className="info">
-            <div className="user">
-                <img src={item.pp} alt="" />
-                <span>{item.username}</span>
-                <div className="star">
-                    <img src="" alt="" />
-                    <span>{item.star}</span>
-                </div>
+          <div className="user">
+            <img src={item.pp} alt="" />
+            <span>{item.username}</span>
+            <div className="star">
+              <AiOutlineStar style={{ color: "yellow" }} />
+              <span>{item.star}</span>
             </div>
-            <p>{item.desc}</p>
+          </div>
+          <p>{item.desc}</p>
         </div>
-        <hr/>
-        <div className="details">
-            <AiOutlineHeart/>
-            <div className="price">
-                <span>STARTING AT</span>
-                <h2>${item.price}</h2>
-            </div>
-          
-        </div>
-    </div>
-    </Link>
-  )
-}
+        <hr />
+        <div className="detail">
+          <AiOutlineHeart />
 
-export default GigCard
+          <h2>${item.price}</h2>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default GigCard;
--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import "./Gigs.scss";
+import { AiOutlineDown } from "react-icons/ai";
+import { useState } from "react";
+import GigCard from "../../components/gigCard/GigCard";
+import { gigs } from "../../data";
 
 const Gigs = () => {
-  return <div>Gigs</div>;
+  const [sort, setSort] = useState("sales");
+  const [hide, setHide] = useState(false);
+
+  const reSortHandler = (type) => {
+    setSort(type);
+    setHide(false);
+  };
+
+  return (
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">FIVER . GRAPHICS & DESIGN</span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with FIverr AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">SortBy </span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}{" "}
+            </span>
+            <AiOutlineDown
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                setHide((prev) => !prev);
+              }}
+            />
+            {hide && (
+              <div className="rightMenu">
+                <span onClick={() => reSortHandler("createdAt")}>Newest</span>
+                <span onClick={() => reSortHandler("sales")}>Best Selling</span>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="cards">
+          {gigs.map((gig) => (
+            <GigCard key={gig.id} item={gig} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gigs;
