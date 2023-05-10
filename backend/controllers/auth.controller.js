1fa6c9b8d799bb907eab5120e6cb56107c20e20e
mishkamag <m93.maglaperidze@gmail.com>
2023-05-10 12:36:05 +0400

    auth routing

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,13 @@
 const User = require("../models/user.model");
+const bcrypt = require('bcrypt');
 
 const userRegister = async (req, res) => {
   try {
-    const newUser = new User(req.body);
+    const hash = bcrypt.hashSync(req.body.password, 5)
+    const newUser = new User({
+      ...req.body,
+      password: hash,
+    });
     await newUser.save();
     res.status(201).send("User has been created");
   } catch (error) {
@@ -10,7 +15,21 @@ const userRegister = async (req, res) => {
   }
 };
 
-const userLogin = (req, res) => {};
+const userLogin = async(req, res) => {
+  try {
+    const user =  await User.findOne({username:req.body.username})
+    if(!user) return res.status(404).send("User not found")
+
+    const correctUser = bcrypt.compareSync(req.body.password, user.password)
+    if(!correctUser) return res.status(400).send("Wrong password or username")
+
+    const {password, ...other} = user._doc
+    res.status(200).send(other)
+
+  } catch (error) {
+    res.status(500).send("Some error")
+  }
+};
 
 const userLogout = (req, res) => {};
 
