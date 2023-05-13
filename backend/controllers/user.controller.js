047d33775ee2b910c06dd2e4dfa350622bedb6ed
mishkamag <m93.maglaperidze@gmail.com>
2023-05-13 12:36:41 +0400

    working to delete user image too

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,13 @@
 const User = require("../models/user.model");
 const { createError } = require("../helpers/createEror");
+// const cloudinary = require('cloudinary').v2
+
+// cloudinary.config({
+//   cloud_name: "dayas7n42",
+//   api_key: "857154711538518",
+//   api_secret: "IpVGMzzmt1IMsIVg3cL6K3ju23g"
+// });
+
 
 const deleteUser = async (req, res, next) => {
   const user = await User.findById(req.params.id);
@@ -7,6 +15,12 @@ const deleteUser = async (req, res, next) => {
   if (req.userId !== user._id.toString()) {
     return next(createError(403, "You only can delete you acc"));
   }
+
+  // if (user.imagePublicId) {
+  //   await cloudinary.uploader.destroy(user.imagePublicId);
+  // }
+
+
   await User.findOneAndDelete(req.params.id);
   res.status(200).send("Acc has been deleted");
 };
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -37,6 +37,10 @@ const UserSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    // imagePublicId: {
+    //   type: String,
+    //   required: false,
+    // },
   },
   {
     timestamps: true,
