f61d89ec3bb8250b52c7f12b23d8ceca949f31b3
mishkamag <m93.maglaperidze@gmail.com>
2023-05-14 22:38:11 +0400

    fixed tables warnings

--- frontend/src/pages/messages/Messages.jsx
+++ frontend/src/pages/messages/Messages.jsx
@@ -10,9 +10,9 @@ const Messages = () => {
   };
 
   const message = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-  maxime cum corporis esse aspernatur laborum dolorum? Animi
-  molestias aliquam, cum nesciunt, aut, ut quam vitae saepe repellat
-  nobis praesentium placeat.`;
+    maxime cum corporis esse aspernatur laborum dolorum? Animi
+    molestias aliquam, cum nesciunt, aut, ut quam vitae saepe repellat
+    nobis praesentium placeat.`;
 
   return (
     <div className="messages">
@@ -21,60 +21,66 @@ const Messages = () => {
           <h1>Messages</h1>
         </div>
         <table>
-          <tr>
-            <th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>
-            <th>Last Message</th>
-            <th>Date</th>
-            <th>Action</th>
-          </tr>
-          <tr className="active">
-            <td>Charley Sharp</td>
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>1 hour ago</td>
-            <td>
-              <button>Mark as Read</button>
-            </td>
-          </tr>
-          <tr className="active">
-            <td>John Doe</td>
-
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>2 hours ago</td>
-            <td>
-              <button>Mark as Read</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Elinor Good</td>
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>1 day ago</td>
-          </tr>
-          <tr>
-            <td>Garner David </td>
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>2 days ago</td>
-          </tr>
-          <tr>
-            <td>Troy Oliver</td>
-            <td>{message.substring(0, 100)}</td>
-            <td>1 week ago</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>
+              <th>Last Message</th>
+              <th>Date</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="active">
+              <td>Charley Sharp</td>
+              <td>
+                <Link to="/message/123" className="link">
+                  {message.substring(0, 100)}...
+                </Link>
+              </td>
+              <td>1 hour ago</td>
+              <td>
+                <button>Mark as Read</button>
+              </td>
+            </tr>
+            <tr className="active">
+              <td>John Doe</td>
+              <td>
+                <Link to="/message/123" className="link">
+                  {message.substring(0, 100)}...
+                </Link>
+              </td>
+              <td>2 hours ago</td>
+              <td>
+                <button>Mark as Read</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Elinor Good</td>
+              <td>
+                <Link to="/message/123" className="link">
+                  {message.substring(0, 100)}...
+                </Link>
+              </td>
+              <td>1 day ago</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Garner David </td>
+              <td>
+                <Link to="/message/123" className="link">
+                  {message.substring(0, 100)}...
+                </Link>
+              </td>
+              <td>2 days ago</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Troy Oliver</td>
+              <td>{message.substring(0, 100)}</td>
+              <td>1 week ago</td>
+              <td></td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
