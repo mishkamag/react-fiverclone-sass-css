dae392444fc93a96fd4f236a365343cca03739f6
mishkamag <m93.maglaperidze@gmail.com>
2023-05-23 20:58:26 +0400

    finished project

--- frontend/src/pages/add/Add.jsx
+++ frontend/src/pages/add/Add.jsx
@@ -1,77 +1,167 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import "./Add.scss";
+import { INITIAL_STATE, gigReducer } from "../../reducers/gigReducer";
+import newRequest from "../../helpers/newRequest";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import upload from "../../helpers/upload";
 
 const Add = () => {
+  const [singleFile, setSingleFile] = useState(undefined);
+  const [files, setFiles] = useState([]);
+  const [uploading, setUploading] = useState(false);
+
+  const [state, dispatch] = useReducer(gigReducer, INITIAL_STATE);
+
+  const handleChange = (e) => {
+    dispatch({
+      type: "CHANGE_INPUT",
+      payload: { name: e.target.name, value: e.target.value },
+    });
+  };
+
+  const handleFeature = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_FEATURE",
+      payload: e.target[0].value,
+    });
+    e.target[0].value = "";
+  };
+
+  const handleUpload = async () => {
+    setUploading(true);
+    try {
+      const cover = await upload(singleFile);
+
+      const images = await Promise.all(
+        [...files].map(async (file) => {
+          const url = await upload(file);
+          return url;
+        })
+      );
+      setUploading(false);
+      dispatch({ type: "ADD_IMAGES", payload: { cover, images } });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const navigate = useNavigate();
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (gig) => {
+      return newRequest.post("/gigs", gig);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["myGigs"]);
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    mutation.mutate(state);
+    navigate("/mygigs");
+  };
+  console.log(state);
+
   return (
     <div className="add">
       <div className="container">
         <h1>Add New Gig</h1>
         <div className="sections">
-          <div className="left">
-            <label htmlFor="title">Title</label>
+          <div className="info">
+            <label htmlFor="">Title</label>
             <input
               type="text"
-              id="title"
+              name="title"
               placeholder="e.g. I will do something I'm really good at"
+              onChange={handleChange}
             />
-            <label htmlFor="category">Category</label>
-            <select name="cats" id="category">
+            <label htmlFor="">Category</label>
+            <select name="cat" id="cat" onChange={handleChange}>
               <option value="design">Design</option>
               <option value="web">Web Development</option>
               <option value="animation">Animation</option>
               <option value="music">Music</option>
             </select>
-            <label htmlFor="coverImage">Cover Image</label>
-            <input type="file" id="coverImage" />
-            <label htmlFor="uploadImages">Upload Images</label>
-            <input type="file" id="uploadImages" multiple />
-            <label htmlFor="description">Description</label>
+            <div className="images">
+              <div className="imagesInputs">
+                <label htmlFor="">Cover Image</label>
+                <input
+                  type="file"
+                  onChange={(e) => setSingleFile(e.target.files[0])}
+                />
+                <label htmlFor="">Upload Images</label>
+                <input
+                  type="file"
+                  multiple
+                  onChange={(e) => setFiles(e.target.files)}
+                />
+              </div>
+              <button onClick={handleUpload}>
+                {uploading ? "uploading" : "Upload"}
+              </button>
+            </div>
+            <label htmlFor="">Description</label>
             <textarea
-              id="description"
-              cols="30"
+              name="desc"
+              id=""
+              placeholder="Brief descriptions to introduce your service to customers"
+              cols="0"
               rows="16"
-              placeholder="Write description"
+              onChange={handleChange}
             ></textarea>
-            <button>Create</button>
+            <button onClick={handleSubmit}>Create</button>
           </div>
-
-          <div className="right">
-            <label htmlFor="serviceTitle">Service Title</label>
+          <div className="details">
+            <label htmlFor="">Service Title</label>
             <input
               type="text"
-              id="serviceTitle"
+              name="shortTitle"
               placeholder="e.g. One-page web design"
+              onChange={handleChange}
             />
-            <label htmlFor="shortDescription">Short Description</label>
+            <label htmlFor="">Short Description</label>
             <textarea
-              id="shortDescription"
+              name="shortDesc"
+              onChange={handleChange}
+              id=""
               placeholder="Short description of your service"
               cols="30"
               rows="10"
             ></textarea>
-            <label htmlFor="deliveryTime">Delivery Time (e.g. 3 days)</label>
-            <input type="number" id="deliveryTime" min={1} />
-            <label htmlFor="revisionNumber">Revision Number</label>
-            <input type="number" id="revisionNumber" min={1} />
-            <label htmlFor="addFeatures">Add Features</label>
+            <label htmlFor="">Delivery Time (e.g. 3 days)</label>
+            <input type="number" name="deliveryTime" onChange={handleChange} />
+            <label htmlFor="">Revision Number</label>
             <input
-              type="text"
-              id="addFeatures"
-              placeholder="e.g. page design"
+              type="number"
+              name="revisionNumber"
+              onChange={handleChange}
             />
-            <input
-              type="text"
-              id="addFeatures"
-              placeholder="e.g. file uploading"
-            />
-            <input
-              type="text"
-              id="addFeatures"
-              placeholder="e.g. setting up a domain"
-            />
-            <input type="text" id="addFeatures" placeholder="e.g. hosting" />
-            <label htmlFor="price">Price</label>
-            <input type="number" id="price" min={1} />
+            <label htmlFor="">Add Features</label>
+            <form action="" className="add" onSubmit={handleFeature}>
+              <input type="text" placeholder="e.g. page design" />
+              <button type="submit">add</button>
+            </form>
+            <div className="addedFeatures">
+              {state?.features?.map((f) => (
+                <div className="item" key={f}>
+                  <button
+                    onClick={() =>
+                      dispatch({ type: "REMOVE_FEATURE", payload: f })
+                    }
+                  >
+                    {f}
+                    <span>X</span>
+                  </button>
+                </div>
+              ))}
+            </div>
+            <label htmlFor="">Price</label>
+            <input type="number" onChange={handleChange} name="price" />
           </div>
         </div>
       </div>
